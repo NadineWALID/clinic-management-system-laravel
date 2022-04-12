43c0ead2529b930db682ca2752d67fe7b51543e4
Nadine Walid <nadeenwaleed60@gmail.com>
2022-04-12 22:24:55 +0200

    appointment modification

--- database/migrations/2021_11_21_233630_create_appointments_table.php
+++ database/migrations/2021_11_21_233630_create_appointments_table.php
@@ -22,8 +22,8 @@ class CreateAppointmentsTable extends Migration
             $table->string('email')->nullable();
             $table->string('phone');
             $table->string('date');
-            $table->dateTime('start');
-            $table->dateTime('end');
+            $table->dateTime('start')->nullable();
+            $table->dateTime('end')->nullable();
             $table->string('time');
             $table->string('gender');
             $table->string('address')->nullable();
